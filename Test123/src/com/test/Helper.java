1065a38df430f2cb634a47ef505fd9cf38c6ac19
neelamm <neelam.maurya87@gmail.com>
2015-10-11 00:16:31 +0200

    test123 Repo E -> G

---
+++ Test123/src/com/test/Helper.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author NEELAM
+ *
+ */
+public class Helper {
+
+	/**
+	 * 
+	 */
+	public Helper() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void helperStr(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helper.helperStr()");
+	}
+
+}
---
+++ Test123/src/com/test/NTesting.java
@@ -0,0 +1,24 @@
+package com.test;
+
+/**
+ * @author NEELAM
+ *
+ */
+public class NTesting {
+
+	/**
+	 * Default Constructor Calling
+	 */
+	public NTesting() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("NTesting.main()");
+	}
+
+}
